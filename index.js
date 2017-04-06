d06781b32dd3f56b5f8707273c9712bc2097119e
Sachin Garg <sachinjnd@gmail.com>
2017-04-07 00:14:10 +0530

    json body support

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express');
-var app = express();
+var bodyParser = require('body-parser');
 var Mailgun = require('mailgun-js');
+
+var app = express();
+app.use(bodyParser.json());       // to support JSON-encoded bodies
 
 app.set('port', (process.env.PORT || 5000));
 
